377dc11f824ef2a8f086353e9b8a95bc222cfced
pangyuteng <pangyuteng+github@gmail.com>
2021-01-18 09:01:27 -0800

    patch - hostname not propagating in .env file

--- up.sh
+++ up.sh
@@ -1,1 +1,1 @@
-docker-compose --env-file .env.prod --file docker-compose.yml --file docker-compose.ohgodpill.yml --build --force-recreate up -d
+WORKERNAME=$HOSTNAME docker-compose --env-file .env --file docker-compose.yml --file docker-compose.ohgodpill.yml up -d --build
